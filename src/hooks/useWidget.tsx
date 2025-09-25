2baace7521dc5c50af2714a4fd797f376b49f0e7
Milos_Petrovic <milos.petrovic@shindiristudio.com>
2025-09-26 01:59:49 +0200

    added closing in history widget wwhen you are on that movie

--- src/hooks/useWidget.tsx
+++ src/hooks/useWidget.tsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 import { useMovieHistory } from "../context/useHistoryContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { PagePaths } from "../routes/routes_utils";
 
 export const useWidget = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const { addToHistory, movieHistory, clearHistory } = useMovieHistory();
+  const navigate = useNavigate();
 
+  const { id: currentId } = useParams<{ id: string }>();
   const toggleExpanded = () => {
     setIsExpanded(!isExpanded);
   };
 
-  const navigate = useNavigate();
-
   const handleMovieClick = (id: number) => {
+    if (currentId === String(id)) {
+      setIsExpanded(false);
+    }
     navigate(`${PagePaths.DETAILS.replace(":id", String(id))}`);
   };
+
   return {
     isExpanded,
     toggleExpanded,
