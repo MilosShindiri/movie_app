41b02b447bfc3d1839c486f61d1f27db17dd8bf5
Milos_Petrovic <milos.petrovic@shindiristudio.com>
2025-09-26 14:20:48 +0200

    refactoring HistoryWidget to use ref

--- src/hooks/useWidget.tsx
+++ src/hooks/useWidget.tsx
@@ -1,28 +1,18 @@
-import { useState } from "react";
 import { useMovieHistory } from "../context/useHistoryContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { PagePaths } from "../routes/routes_utils";
 
 export const useWidget = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
   const { addToHistory, movieHistory, clearHistory } = useMovieHistory();
   const navigate = useNavigate();
-
   const { id: currentId } = useParams<{ id: string }>();
-  const toggleExpanded = () => {
-    setIsExpanded(!isExpanded);
-  };
 
   const handleMovieClick = (id: number) => {
-    if (currentId === String(id)) {
-      setIsExpanded(false);
-    }
+    if (currentId === String(id)) return;
     navigate(`${PagePaths.DETAILS.replace(":id", String(id))}`);
   };
 
   return {
-    isExpanded,
-    toggleExpanded,
     handleMovieClick,
     addToHistory,
     movieHistory,
